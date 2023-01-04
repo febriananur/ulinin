6161270fdefa02e05d9c3609e5e5925429ef3159
febriananur <febriananurokhman@gmail.com>
2023-01-04 21:44:36 +0700

    migration wisata

---
+++ database/migrations/2023_01_03_134904_create_wisatas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wisatas', function (Blueprint $table) {
+            $table->bigIncrements('id_wisata');
+            $table->string('wisata');
+            $table->integer('id_kategori');
+            $table->string('lokasi');
+            $table->string('gambar');
+            $table->text('deskripsi');
+            $table->integer('id_mitra');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wisatas');
+    }
+};
